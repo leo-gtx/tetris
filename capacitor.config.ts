02e44fbd4f4e29efaac046d51e9fe8c45f7ec6a1
leo-gtx <nguefackleonel@gmail.com>
2024-08-14 12:33:13 +0100

    commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ndlpixel.tetris',
+  appName: 'Tetris',
+  webDir: 'public'
+};
+
+export default config;
